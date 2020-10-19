cecd2167338f93253f964bcbdd709c8dabe9bbbe
Jakob Lehr <jakob@myhd.org>
2020-10-19 16:36:18 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+// Define spreadsheet URL.
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1qT1LyvoAcb0HTsi2rHBltBVpUBumAUzT__rhMvrz5Rk/edit#gid=0';
+// Load an entire worksheet.
+$('#statistics').sheetrock({
+  url: mySpreadsheet
+});
